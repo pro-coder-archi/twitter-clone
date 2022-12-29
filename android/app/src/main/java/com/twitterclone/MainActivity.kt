80026aa6975e0c6f7f2e52bc3acb52b152e58e18
Archisman-Mridha <archismanmridha12345@gmail.com>
2022-12-29 11:10:04 +0530

    minor path refactoring

--- android/app/src/main/java/com/twitterclone/MainActivity.kt
+++ android/app/src/main/java/com/twitterclone/MainActivity.kt
@@ -1,9 +1,9 @@
-package com.example.twitterclone
+package com.twitterclone
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.twitterclone.theme.AppTheme
+import com.twitterclone.theme.AppTheme
 
 class MainActivity : ComponentActivity( ) {
     override fun onCreate(savedInstanceState: Bundle?) {
